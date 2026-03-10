cee00207e4bb81d6d4fa3662599e936f6d870b1c
RanderDLemos <162617589+RanderDLemos@users.noreply.github.com>
2026-03-10 12:26:37 -0300

    Arma class

---
+++ Aula-04/Ex5/Arma.java
@@ -0,0 +1,17 @@
+public class Arma {
+
+    String nome;
+    int poder;
+    int resistencia;
+    String descricao;
+
+    void mostraInfoArma(){
+
+        System.out.println("Nome da arma: " + nome);
+        System.out.println("Poder da arma: " + poder);
+        System.out.println("Resistencia da arma: " + resistencia);
+        System.out.println("Descricao da arma: " + descricao);
+
+    }
+
+}
