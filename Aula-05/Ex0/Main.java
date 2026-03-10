25ea4360b067db245fcee3e201fd547a5a4b4683
RanderDLemos <162617589+RanderDLemos@users.noreply.github.com>
2026-03-10 16:27:47 -0300

    Create Main.java

---
+++ Aula-05/Ex0/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args){
+
+        Cantina cantina = new Cantina();
+        cantina.nome = "Cantina do Inatel";
+
+        Salgado s1 = new Salgado();
+        Salgado s2 = new Salgado();
+        Salgado s3 = new Salgado();
+
+        s1.nome = "empada";
+        s2.nome = "pastel";
+        s3.nome = "esfirra";
+
+        cantina.addSalgado(s1);
+        cantina.addSalgado(s2);
+        cantina.addSalgado(s3);
+
+        cantina.mostraInfo();
+
+    }
+}
