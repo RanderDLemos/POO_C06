70525066515add9e32daf6b7cc0c02616b29d02d
RanderDLemos <162617589+RanderDLemos@users.noreply.github.com>
2026-03-10 16:00:35 -0300

    main

---
+++ Aula-05/Ex1_Desafio/main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class main {
+
+    public static void main(String [] args){
+
+        boolean verificar;
+        Bomba jogo = new Bomba();
+
+        System.out.println("Digite qual linha voce deseja caminhar");
+        Scanner entrada1 = new Scanner(System.in);
+        int digito1 = entrada1.nextInt();
+        System.out.println("Digite qual coluna voce deseja caminhar");
+        Scanner entrada2 = new Scanner(System.in);
+        int digito2 = entrada2.nextInt();
+
+        verificar = jogo.inicializarJogo(digito1, digito2);
+        if(verificar){
+            System.out.println("Voce ganhou");
+        }else {
+            System.out.println("Voce perdeu");
+        }
+
+    }
+
+}
